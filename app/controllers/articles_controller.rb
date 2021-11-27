37a8269fc820ad522c099df574245ec522d0576a
areeshz <azindani786@gmail.com>
2021-11-26 22:33:30 -0500

    Add index route and action for articles resource

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		# byebug # used to stop execution and debug within the server console
 	end
+
+	def index
+		@articles = Article.all
+	end
 end
