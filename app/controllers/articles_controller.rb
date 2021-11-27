d50b5ea072e1aa90fc5f1f5a84a9d19721a0aba0
areeshz <azindani786@gmail.com>
2021-11-27 13:02:52 -0500

    Add 'new' and 'create' routes and actions for Articles, with a form for creating new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,17 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def new
+	end
+
+	def create
+		# render plain: params[:article] # this can be used to plainly render this value back on the page, as another debugging tool
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		# render plain: @article.inspect
+		@article.save
+		# redirect_to article_path(@article)
+		# as an alternative shorthand, you can do the following and rails will know to redirect to that article
+		redirect_to @article
+	end
 end
