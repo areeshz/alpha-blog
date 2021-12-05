2ca49b5d41322ea8d39534cb298cf9ab32204097
areeshz <azindani786@gmail.com>
2021-12-04 21:25:36 -0500

    Add one-to-many association between articles and users; Temporarily assign all articles to be owned by the first user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ApplicationRecord
 	validates :title, presence: true, length: {minimum: 6, maximum: 100}
 	validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
+	# ":user" must be singular for this to work, not ":users"
+	# in rails 5 and onwards, having a 'belongs_to' association
+	# automatically makes that reference required.
+	# So an article must have a user.
+	# It can be made optional with "belongs_to :user, optional: true"
+	belongs_to :user
 end
